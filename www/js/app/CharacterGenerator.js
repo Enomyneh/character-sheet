71a720e9b6440ec4b2371c34052ee2717f8da373
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-11-09 21:07:37 +1000

    Health, Mana, Willpower, Wisdom added

--- www/js/app/CharacterGenerator.js
+++ www/js/app/CharacterGenerator.js
@@ -10,7 +10,7 @@ define([
     'json!data/flaws.json',
     'json!data/merits.json',
     'json!data/arcana.json'
-], function(
+], function (
     Character,
     Chance,
     attributes,
@@ -23,80 +23,89 @@ define([
     merits,
     arcana) {
 
-    function CharacterGenerator() {
-        console.log("Initialising CharacterGenerator definition");
+        function CharacterGenerator() {
+            console.log("Initialising CharacterGenerator definition");
 
-        // methods
-        this.initialize = function() {
-            console.log("Creating CharacterGenerator");
-        };
+            // methods
+            this.initialize = function () {
+                console.log("Creating CharacterGenerator");
+            };
 
-        this.generateRandomCharacter = function() {
-            console.log("Generating random character");
+            this.generateRandomCharacter = function () {
+                console.log("Generating random character");
 
-            var character = new Character();
-            character.gender = chance.gender();
+                var character = new Character();
+                character.gender = chance.gender();
 
-            character.nameParts = {};
-            character.nameParts.prefix = '';
-            // character.nameParts.prefix = chance.prefix({ gender: character.gender });
-            character.nameParts.first = chance.first({ gender: character.gender });
-            character.nameParts.last = chance.last();
-            character.nameParts.suffix = '';
-            // character.nameParts.suffix = chance.suffix();
+                character.nameParts = {};
+                character.nameParts.prefix = '';
+                // character.nameParts.prefix = chance.prefix({ gender: character.gender });
+                character.nameParts.first = chance.first({ gender: character.gender });
+                character.nameParts.last = chance.last();
+                character.nameParts.suffix = '';
+                // character.nameParts.suffix = chance.suffix();
 
-            character.name = (character.nameParts.prefix + ' ' + character.nameParts.first + ' ' + character.nameParts.last + ' ' + character.nameParts.suffix).trim();
-            character.birthday = chance.birthday();
-            character.countryOfBirth = chance.country({ full: true });
-            character.age = Math.floor((Date.now() - character.birthday) / (1000 * 60 * 60 * 24 * 365));
+                character.name = (character.nameParts.prefix + ' ' + character.nameParts.first + ' ' + character.nameParts.last + ' ' + character.nameParts.suffix).trim();
+                character.birthday = chance.birthday();
+                character.countryOfBirth = chance.country({ full: true });
+                character.age = Math.floor((Date.now() - character.birthday) / (1000 * 60 * 60 * 24 * 365));
 
-            character.concept = chance.profession();
+                character.concept = chance.profession();
 
-            character.cabal = chance.company();
+                character.cabal = chance.company();
 
-            character.path = chance.pickone(paths).name;
-            character.order = chance.pickone(orders).name;
+                character.path = chance.pickone(paths).name;
+                character.order = chance.pickone(orders).name;
 
-            character.virtue = chance.pickone(virtues).name;
-            character.vice = chance.pickone(vices).name;
+                character.virtue = chance.pickone(virtues).name;
+                character.vice = chance.pickone(vices).name;
 
-            character.flaws = chance.pickset(flaws, chance.integer({ min: 0, max: 2 })).map(function(flaw) {
-                return {
-                    name: flaw.name
-                };
-            });
+                character.wisdom = chance.integer({ min: 1, max: 10 });
 
-            character.merits = chance.pickset(merits, chance.integer({ min: 0, max: 7 })).map(function(merit) {
-                return {
-                    name: merit.name,
-                    dots: 1
-                };
-            });
+                // Starting Values
+                character.bashingDamage = chance.integer({ min: 0, max: 2 });
+                character.lethalDamage = chance.integer({ min: 0, max: 2 });
+                character.aggravatedDamage = chance.integer({ min: 0, max: 2 });
+                character.willpower = chance.integer({ min: 0, max: character.maxWillpower() });
+                character.mana = chance.integer({ min: 0, max: character.maxMana() });
 
-            // Attributes
-            attributes.forEach(function(attribute) {
-                character[attribute.name.toLowerCase()] = chance.integer({ min: 1, max: 5 });
-            }, this);
+                character.flaws = chance.pickset(flaws, chance.integer({ min: 0, max: 2 })).map(function (flaw) {
+                    return {
+                        name: flaw.name
+                    };
+                });
 
-            // Skills
-            skills.forEach(function(skill) {
-                character[skill.name.toLowerCase()] = chance.integer({ min: 0, max: 5 });
-                character[skill.name.toLowerCase()].specialities = chance.pickset(skill.specialties, chance.integer({ min: 0, max: 2 }))
-            }, this);
+                character.merits = chance.pickset(merits, chance.integer({ min: 0, max: 7 })).map(function (merit) {
+                    return {
+                        name: merit.name,
+                        dots: 1
+                    };
+                });
 
-            // Arcana
-            arcana.forEach(function(arcanum) {
-                character[arcanum.name.toLowerCase()] = chance.integer({ min: 0, max: 5 });
-            }, this);
+                // Attributes
+                attributes.forEach(function (attribute) {
+                    character[attribute.name.toLowerCase()] = chance.integer({ min: 1, max: 5 });
+                }, this);
+
+                // Skills
+                skills.forEach(function (skill) {
+                    character[skill.name.toLowerCase()] = chance.integer({ min: 0, max: 5 });
+                    character[skill.name.toLowerCase()].specialities = chance.pickset(skill.specialties, chance.integer({ min: 0, max: 2 }))
+                }, this);
+
+                // Arcana
+                arcana.forEach(function (arcanum) {
+                    character[arcanum.name.toLowerCase()] = chance.integer({ min: 0, max: 5 });
+                }, this);
 
 
-            return character;
+                return character;
 
-        };
+            };
 
-        // Initialise
-        this.initialize();
-    }
+            // Initialise
+            this.initialize();
+        }
 
-    return new CharacterGenerator();
-});
+        return new CharacterGenerator();
+    });
--- www/js/app/models/Character.js
+++ www/js/app/models/Character.js
@@ -2,87 +2,96 @@ define([
     'json!data/attributes.json',
     'json!data/skills.json',
     'json!data/arcana.json'
-], function(
+], function (
     attributes,
     skills,
     arcana) {
 
-    console.log("Initialising Character definition");
+        console.log("Initialising Character definition");
 
-    // This is specificallly a Mage the Awakening Character
-    function Character(character) {
+        // This is specificallly a Mage the Awakening Character
+        function Character(character) {
 
-        this.initialize = function() {
-            console.log("Creating Character");
-        };
+            this.initialize = function (character) {
 
-        if (character) {
-            for (var property in character) {
-                if (character.hasOwnProperty(property)) {
-                    this[property] = character[property];
+                console.log("Creating Character");
+
+                if (character) {
+                    for (var property in character) {
+                        if (character.hasOwnProperty(property)) {
+                            this[property] = character[property];
+                        }
+                    }
+                } else {
+
+                    this.name = "";
+                    this.age = 0;
+                    this.player = "Your name here";
+                    this.chronicle = "";
+                    this.concept = "";
+                    this.virtue = "";
+                    this.vice = "";
+
+                    // Skills
+                    skills.forEach(function (skill) {
+                        this[skill.name.toLowerCase()] = 0;
+                    }, this);
+
+                    // Attributes
+                    attributes.forEach(function (attribute) {
+                        this[attribute.name.toLowerCase()] = 1;
+                    }, this);
+
+                    // Aracana
+                    arcana.forEach(function (arcanum) {
+                        this[arcanum.name.toLowerCase()] = 0;
+                    }, this);
+
+                    this.flaws = [];
+                    this.merits = [];
+
+                    this.size = 5;
+                    this.gnosis = 1;
+                    this.wisdom = 7;
+                    this.experience = 0;
+
+                    // Starting Values
+                    this.bashingDamage = 0;
+                    this.lethalDamage = 0;
+                    this.aggravatedDamage = 0;
+                    this.willpower = this.maxWillpower();
+                    this.mana = this.wisdom;
+
                 }
-            }
-        } else {
+            };
 
-            this.name = "";
-            this.age = 0;
-            this.player = "Your name here";
-            this.chronicle = "";
-            this.concept = "";
-            this.virtue = "";
-            this.vice = "";
+            this.maxHealth = function () {
+                return this.size + this.stamina;
+            };
 
-            // Skills
-            skills.forEach(function(skill) {
-                this[skill.name.toLowerCase()] = 0;
-            }, this);
+            this.maxWillpower = function () {
+                return this.resolve + this.composure;
+            };
 
-            // Attributes
-            attributes.forEach(function(attribute) {
-                this[attribute.name.toLowerCase()] = 1;
-            }, this);
+            this.maxMana = function () {
+                // TODO work out calculation for this.
+                return 10;
+            };
 
-            // Aracana
-            arcana.forEach(function(arcanum) {
-                this[arcanum.name.toLowerCase()] = 0;
-            }, this);
+            this.defense = function () {
+                return Math.min(this.dexterity, this.wits);
+            };
 
-            this.flaws = [];
-            this.merits = [];
+            this.initiativeMod = function () {
+                return this.dexterity + this.composure;
+            };
 
-            this.size = 5;
-            this.gnosis = 1;
-            this.wisdom = 7;
-            this.experience = 0;
+            this.speed = function () {
+                return this.dexterity + this.strength + 5;
+            };
 
+            this.initialize(character);
         }
 
-        this.maxHealth = function() {
-            return this.size + this.stamina;
-        };
-
-        this.maxWillpower = function() {
-            return this.resolve + this.composure;
-        };
-
-        this.maxMana = function() {
-            return this.size + this.stamina;
-        };
-
-        this.defense = function() {
-            return Math.min(this.dexterity, this.wits);
-        };
-
-        this.initiativeMod = function() {
-            return this.dexterity + this.composure;
-        };
-
-        this.speed = function() {
-            return this.dexterity + this.strength + 5;
-        };
-
-        this.initialize();
-    }
-
-    return Character;
-});
+        return Character;
+    });
