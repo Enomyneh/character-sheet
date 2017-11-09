2ada54eb2599c8d44b40a0fe7a89549ef3cb3121
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-11-09 21:42:34 +1000

    Wisdom functional

--- www/js/app/models/Character.js
+++ www/js/app/models/Character.js
@@ -2,96 +2,97 @@ define([
     'json!data/attributes.json',
     'json!data/skills.json',
     'json!data/arcana.json'
-], function (
+], function(
     attributes,
     skills,
     arcana) {
 
-        console.log("Initialising Character definition");
+    console.log("Initialising Character definition");
 
-        // This is specificallly a Mage the Awakening Character
-        function Character(character) {
+    // This is specificallly a Mage the Awakening Character
+    function Character(character) {
 
-            this.initialize = function (character) {
+        this.initialize = function(character) {
 
-                console.log("Creating Character");
+            console.log("Creating Character");
 
-                if (character) {
-                    for (var property in character) {
-                        if (character.hasOwnProperty(property)) {
-                            this[property] = character[property];
-                        }
+            if (character) {
+                for (var property in character) {
+                    if (character.hasOwnProperty(property)) {
+                        this[property] = character[property];
                     }
-                } else {
-
-                    this.name = "";
-                    this.age = 0;
-                    this.player = "Your name here";
-                    this.chronicle = "";
-                    this.concept = "";
-                    this.virtue = "";
-                    this.vice = "";
-
-                    // Skills
-                    skills.forEach(function (skill) {
-                        this[skill.name.toLowerCase()] = 0;
-                    }, this);
-
-                    // Attributes
-                    attributes.forEach(function (attribute) {
-                        this[attribute.name.toLowerCase()] = 1;
-                    }, this);
-
-                    // Aracana
-                    arcana.forEach(function (arcanum) {
-                        this[arcanum.name.toLowerCase()] = 0;
-                    }, this);
-
-                    this.flaws = [];
-                    this.merits = [];
-
-                    this.size = 5;
-                    this.gnosis = 1;
-                    this.wisdom = 7;
-                    this.experience = 0;
-
-                    // Starting Values
-                    this.bashingDamage = 0;
-                    this.lethalDamage = 0;
-                    this.aggravatedDamage = 0;
-                    this.willpower = this.maxWillpower();
-                    this.mana = this.wisdom;
-
                 }
-            };
+            } else {
 
-            this.maxHealth = function () {
-                return this.size + this.stamina;
-            };
+                this.name = "";
+                this.age = 0;
+                this.player = "Your name here";
+                this.chronicle = "";
+                this.concept = "";
+                this.virtue = "";
+                this.vice = "";
 
-            this.maxWillpower = function () {
-                return this.resolve + this.composure;
-            };
+                // Skills
+                skills.forEach(function(skill) {
+                    this[skill.name.toLowerCase()] = 0;
+                }, this);
 
-            this.maxMana = function () {
-                // TODO work out calculation for this.
-                return 10;
-            };
+                // Attributes
+                attributes.forEach(function(attribute) {
+                    this[attribute.name.toLowerCase()] = 1;
+                }, this);
 
-            this.defense = function () {
-                return Math.min(this.dexterity, this.wits);
-            };
+                // Aracana
+                arcana.forEach(function(arcanum) {
+                    this[arcanum.name.toLowerCase()] = 0;
+                }, this);
 
-            this.initiativeMod = function () {
-                return this.dexterity + this.composure;
-            };
+                this.flaws = [];
+                this.merits = [];
+                this.derangements = [];
 
-            this.speed = function () {
-                return this.dexterity + this.strength + 5;
-            };
+                this.size = 5;
+                this.gnosis = 1;
+                this.wisdom = 7;
+                this.experience = 0;
 
-            this.initialize(character);
-        }
+                // Starting Values
+                this.bashingDamage = 0;
+                this.lethalDamage = 0;
+                this.aggravatedDamage = 0;
+                this.willpower = this.maxWillpower();
+                this.mana = this.wisdom;
 
-        return Character;
-    });
+            }
+        };
+
+        this.maxHealth = function() {
+            return this.size + this.stamina;
+        };
+
+        this.maxWillpower = function() {
+            return this.resolve + this.composure;
+        };
+
+        this.maxMana = function() {
+            // TODO work out calculation for this.
+            return 10;
+        };
+
+        this.defense = function() {
+            return Math.min(this.dexterity, this.wits);
+        };
+
+        this.initiativeMod = function() {
+            return this.dexterity + this.composure;
+        };
+
+        this.speed = function() {
+            return this.dexterity + this.strength + 5;
+        };
+
+        this.initialize(character);
+    }
+
+    return Character;
+});
